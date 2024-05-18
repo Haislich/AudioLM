2bb2ef66e99287d91dd219421d258e3195d855b9
Haislich <josedlv936@gmail.com>
2024-05-18 03:13:27 +0200

    Introduded some tests.

--- src/audiolm/transformer/trainer.py
+++ src/audiolm/transformer/trainer.py
@@ -11,10 +11,12 @@ from torch import nn
 from tqdm.auto import tqdm
 
 from audiolm.acoustic_modelling.custom_encodec import CustomEncodecModel
-from audiolm.costants import DEVICE
+from audiolm.costants import DEVICE, DEBUG
 from audiolm.data_preparation import AudioDataLoader
 from audiolm.semantic_acoustic_modeling.W2VHuBERT_Quantizier import W2VHuBERT_Quantizier
 from audiolm.transformer.AbsoluteTransformer import TransformerDecoderOnly
+
+# TODO Fix docstrings.
 
 
 class Trainer(ABC):
@@ -35,7 +37,7 @@ class Trainer(ABC):
         train_dataloader: Optional[AudioDataLoader] = None,
         val_dataloader: Optional[AudioDataLoader] = None,
         test_dataloader: Optional[AudioDataLoader] = None,
-        loss: Optional[torch.nn.Module] = None,
+        loss: Optional[nn.Module] = None,
         optimizer: Optional[torch.optim.Optimizer] = None,
         intervals: Optional[int] = None,
         save_path: Optional[Path] = None,
@@ -61,15 +63,56 @@ class Trainer(ABC):
         if save_path is not None and not os.path.exists(self.save_path):
             os.makedirs(self.save_path)
 
+    # region: Abstract methods, this methods must be redefined accordingly.
     @abstractmethod
-    def _loss_generator(self, batch):
-        pass
+    def loss_generator(self, batch):
+        """Generate loss"""
 
-    def _train(self):
+    @abstractmethod
+    def train(self):
         """
         Train the Transformer model.
         """
-        for epoch in self.epochs:
+
+    @abstractmethod
+    def validate(self):
+        """Test the model on the validation dataset."""
+
+    @abstractmethod
+    def test(self):
+        """Test the model on the test dataset."""
+
+    @abstractmethod
+    def save_checkpoint(self, epoch):
+        """
+        Save a checkpoint of the model and optimizer.
+
+        Args:
+            epoch (int): The current epoch.
+        """
+
+    @abstractmethod
+    def save_model(self):
+        """Save the current model."""
+
+    @abstractmethod
+    def load_checkpoint(self, epoch):
+        """
+        Load a checkpoint of the model and optimizer.
+
+        Args:
+            checkpoint_path (str): The path to the checkpoint file.
+
+        Returns:
+            int: The epoch from which training will resume.
+        """
+
+    # endregion
+
+    # region: Private methods.
+    def _train(self, model: nn.Module):
+        model.train()
+        for epoch in range(self.epochs):
             epoch_loss = 0
             for batch in tqdm(
                 self.train_dataloader,
@@ -79,156 +122,149 @@ class Trainer(ABC):
                 ),
             ):
                 batch = batch.to(DEVICE)
-                output, target = self._loss_generator(batch)
+                print(f"Batch moved to device: {DEVICE}" if DEBUG else "")
+
+                output, target = self.loss_generator(batch)
+                print(
+                    f"Generate output: {output.shape} and target:{target.shape}"
+                    if DEBUG
+                    else ""
+                )
+
                 loss = self.loss(target, output)
+                print(f"Loss: {loss.item()}" if DEBUG else "")
+
                 epoch_loss += loss.item()
 
                 self.optimizer.zero_grad()
 
                 self.loss.backward()
+                print("Loss backward" if DEBUG else "")
 
                 self.optimizer.step()
+                print("Optimizer step" if DEBUG else "")
+
             avg_epoch_loss = epoch_loss / len(self.train_dataloader)
-            print(f"Epoch: {epoch+1}/{self.epochs} | Loss: {avg_epoch_loss: .4f}")
+            print(
+                f"Epoch: {epoch+1}/{self.epochs} | Loss: {avg_epoch_loss: .4f}"
+                if DEBUG
+                else ""
+            )
 
-            # self.save_checkpoint(epoch)
+            self.save_checkpoint(epoch)
+            print(f"Saving loss: {loss}" if DEBUG else "")
+            validation_loss = self._validate(model)
 
-    # def evaluate(self):
-    #     """
-    #     Evaluate the Transformer model on the validation data.
+            if validation_loss < self.best_val_loss:
+                self.best_val_loss = validation_loss
+                self.early_stop_counter = 0
+            else:
+                self.early_stop_counter += 1
 
-    #     Returns:
-    #         float: The average validation loss.
-    #     """
-    #     self.model().eval()
-    #     validation_loss = 0
-    #     with torch.no_grad:
-    #         for batch in enumerate(
-    #             tqdm(
-    #                 self.val_dataloader,
-    #                 total=ceil(
-    #                     len(self.val_dataloader) / self.val_dataloader.batch_size
-    #                 ),
-    #             )
-    #         ):
-    #             batch = batch.to(self.device)
-    #             semantic_token_batch = self.quantizier.forward(batch)
+            if self.early_stop_counter >= self.early_stopping_range:
+                print(f"Early Stopping at epoch: {epoch+1}")
+                break
 
-    #             # teacher forcing, shift the input by one position in order to predict the next token
-    #             input = semantic_token_batch[:, :-1].to(self.device)
-    #             target = semantic_token_batch[:, 1:].to(self.device)
+    def _validate(self, model):
 
-    #             # generate causal mask to prevent the model from attending to future tokens
-    #             causal_mask = self.model.generate_causal_mask(seq_len=input.size(1)).to(
-    #                 self.device
-    #             )
+        model().eval()
+        validation_loss = 0
+        with torch.no_grad:
+            for batch in tqdm(
+                self.val_dataloader,
+                total=ceil(len(self.val_dataloader) / self.val_dataloader.batch_size),
+            ):
+                batch = batch.to(DEVICE)
+                print("Moved batch to device." if DEBUG else "")
 
-    #             # forward pass
-    #             output = self.model(input, tgt_mask=causal_mask)
-    #             output = output.reshape(-1, output.size(-1))
-    #             target = target.reshape(-1)
+                output, target = self.loss_generator(batch)
+                print(
+                    f"Generate output: {output.shape} and target:{target.shape}"
+                    if DEBUG
+                    else ""
+                )
 
-    #             loss = self.loss_function(output, target)
-    #             validation_loss += loss.item()
+                loss = self.loss(output, target)
+                print(f"Loss: {loss.item()}" if DEBUG else "")
 
-    #     avg_val_loss = validation_loss / len(self.val_dataloader)
-    #     print(f"Validation Loss: {avg_val_loss: .4f}")
+                validation_loss += loss.item()
 
-    #     return avg_val_loss
+        avg_val_loss = validation_loss / len(self.val_dataloader)
+        print(f"Validation Loss: {avg_val_loss: .4f}" if DEBUG else "")
 
-    # def test(self):
-    #     """
-    #     Test the Transformer model on the test data.
+        return avg_val_loss
 
-    #     Args:
-    #         test_dataloader (AudioDataLoader): The dataloader for the test data.
-    #     """
-    #     self.model.eval()
-    #     test_loss = 0
-    #     with torch.no_grad():
-    #         for batch in enumerate(
-    #             tqdm(
-    #                 self.test_dataloader,
-    #                 total=ceil(
-    #                     len(self.test_dataloader) / self.test_dataloader.batch_size
-    #                 ),
-    #             )
-    #         ):
-    #             batch = batch.to(self.device)
-    #             semantic_token_batch = self.quantizier.forward(batch)
+    def _test(self, model):
+        model.eval()
+        test_loss = 0
+        with torch.no_grad():
+            for batch in enumerate(
+                tqdm(
+                    self.test_dataloader,
+                    total=ceil(
+                        len(self.test_dataloader) / self.test_dataloader.batch_size
+                    ),
+                )
+            ):
+                batch = batch.to(DEVICE)
+                output, target = self.loss_generator(batch)
 
-    #             # teacher forcing, shift the input by one position in order to predict the next token
-    #             input = semantic_token_batch[:, :-1].to(self.device)
-    #             target = semantic_token_batch[:, 1:].to(self.device)
+                loss = self.loss(output, target)
+                test_loss += loss.item()
 
-    #             # generate causal mask to prevent the model from attending to future tokens
-    #             causal_mask = self.model.generate_causal_mask(seq_len=input.size(1)).to(
-    #                 self.device
-    #             )
+        avg_test_loss = test_loss / len(self.test_dataloader)
+        print(f"Test Loss: {avg_test_loss: .4f}")
 
-    #             output = self.model(input, tgt_mask=causal_mask)
-    #             output = output.reshape(-1, output.size(-1))
-    #             target = target.reshape(-1)
+        return avg_test_loss
 
-    #             loss = self.loss_function(output, target)
-    #             test_loss += loss.item()
+    def _save_checkpoint(self, epoch, model):
 
-    #     avg_test_loss = test_loss / len(self.test_dataloader)
-    #     print(f"Test Loss: {avg_test_loss: .4f}")
+        model_name = type(model).__name__
+        print(f"Model Name {model_name}" if DEBUG else "")
 
-    #     return avg_test_loss
+        checkpoint_path = os.path.join(
+            self.save_path, f"{model_name}_epoch_{epoch+1}.pth"
+        )
+        torch.save(
+            {
+                "epoch": epoch,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+                "best_val_loss": self.best_val_loss,
+                "early_stop_counter": self.early_stop_counter,
+            },
+            checkpoint_path,
+        )
+        print(f"Checkpoint saved: {checkpoint_path}")
 
-    # def save_checkpoint(self, epoch):
-    #     """
-    #     Save a checkpoint of the model and optimizer.
+    def _load_checkpoint(self, model, epoch):
 
-    #     Args:
-    #         epoch (int): The current epoch.
-    #     """
-    #     checkpoint_path = os.path.join(self.save_path, f"model_epoch_{epoch+1}.pth")
-    #     torch.save(
-    #         {
-    #             "epoch": epoch,
-    #             "model_state_dict": self.model.state_dict(),
-    #             "optimizer_state_dict": self.optimizer.state_dict(),
-    #             "scheduler_state_dict": (
-    #                 self.scheduler.state_dict() if self.scheduler else None
-    #             ),
-    #             "best_val_loss": self.best_val_loss,
-    #             "early_stop_counter": self.early_stop_counter,
-    #         },
-    #         checkpoint_path,
-    #     )
-    #     print(f"Checkpoint saved: {checkpoint_path}")
+        model_name = type(model).__name__
+        print(f"Model Name {model_name}" if DEBUG else "")
+        checkpoint_path = os.path.join(
+            self.save_path, f"{model_name}_epoch_{epoch+1}.pth"
+        )
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint["model_state_dict"])
+        self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+        self.best_val_loss = checkpoint["best_val_loss"]
+        self.early_stop_counter = checkpoint["early_stop_counter"]
+        print(
+            f"Checkpoint loaded: {checkpoint_path}, starting from epoch: {checkpoint['epoch']+1}"
+        )
+        return model, checkpoint["epoch"] + 1
 
-    # def load_checkpoint(self, checkpoint_path):
-    #     """
-    #     Load a checkpoint of the model and optimizer.
+    def _save_model(self, model):
+        model_path = self.save_path / type(model).__name__
+        torch.save(model.state_dict(), model_path)
+        print(f"Model saved: {model_path}")
 
-    #     Args:
-    #         checkpoint_path (str): The path to the checkpoint file.
-
-    #     Returns:
-    #         int: The epoch from which training will resume.
-    #     """
-    #     checkpoint = torch.load(checkpoint_path)
-    #     self.model.load_state_dict(checkpoint["model_state_dict"])
-    #     self.optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
-    #     if self.scheduler and checkpoint["scheduler_state_dict"]:
-    #         self.scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
-    #     self.best_val_loss = checkpoint["best_val_loss"]
-    #     self.early_stop_counter = checkpoint["early_stop_counter"]
-    #     print(
-    #         f"Checkpoint loaded: {checkpoint_path}, starting from epoch: {checkpoint['epoch']+1}"
-    #     )
-    #     return checkpoint["epoch"] + 1
-
-    # def save_model(self, path):
-    #     torch.save(self.model.state_dict(), path)
-    #     print(f"Model saved: {path}")
+    # endregion
 
 
 class SemanticTrainer(Trainer):
+    """Trainer class derived from `Trainer`."""
+
     def __init__(
         self,
         semantic_encoder: W2VHuBERT_Quantizier,
@@ -236,7 +272,7 @@ class SemanticTrainer(Trainer):
         train_dataloader: AudioDataLoader,
         val_dataloader: AudioDataLoader,
         test_dataloader: AudioDataLoader,
-        loss: torch.Module,
+        loss: nn.Module,
         optimizer: torch.optim.Optimizer,
         intervals: int,
         save_path: Path,
@@ -244,26 +280,170 @@ class SemanticTrainer(Trainer):
         early_stopping_range: int,
         epochs: int,
     ):
+        """
+        Takes as input `semantic_encoder` and `semantic_transformer`.
+        They determine the `semantic_modelling`.
+
+        `semantic_encoder` must be trained ahead of time, this trainer only
+        trains `semantic_transformer`.
+
+        Args
+        ----
+            `semantic_encoder` (W2VHuBERT_Quantizier)
+
+            `semantic_transformer` (TransformerDecoderOnly)
+
+            `train_dataloader` (AudioDataLoader)
+
+            `val_dataloader` (AudioDataLoader)
+
+            `test_dataloader` (AudioDataLoader)
+
+            `loss` (nn.Module)
+
+            `optimizer` (torch.optim.Optimizer)
+
+            `intervals` (int)
+
+            `save_path` (Path)
+
+            `early_stop_counter` (int)
+
+            `early_stopping_range` (int)
+
+            `epochs` (int)
+        """
         super().__init__(
-            semantic_encoder,
-            semantic_transformer,
-            train_dataloader,
-            val_dataloader,
-            test_dataloader,
-            loss,
-            optimizer,
-            intervals,
-            save_path,
-            early_stop_counter,
-            early_stopping_range,
-            epochs,
+            semantic_encoder=semantic_encoder,
+            semantic_transformer=semantic_transformer,
+            train_dataloader=train_dataloader,
+            val_dataloader=val_dataloader,
+            test_dataloader=test_dataloader,
+            loss=loss,
+            optimizer=optimizer,
+            intervals=intervals,
+            save_path=save_path,
+            early_stop_counter=early_stop_counter,
+            early_stopping_range=early_stopping_range,
+            epochs=epochs,
         )
 
-    @Trainer._train
-    def train(self):
-        self._train()
-        return
-
-    def _loss_generator(self, batch):
+    def loss_generator(self, batch):
         semantic_encode = self.semantic_encoder(batch)
         return self.semantic_transformer.fit(semantic_encode)
+
+    def train(self):
+        return self._train(self.semantic_transformer)
+
+    def validate(self):
+        return self._validate(self.semantic_transformer)
+
+    def test(self):
+        return self._test(self.semantic_transformer)
+
+    def save_checkpoint(self, epoch):
+        return self._save_checkpoint(self.semantic_transformer, epoch)
+
+    def load_checkpoint(self, epoch):
+        return self._load_checkpoint(self.semantic_transformer, epoch)
+
+    def save_model(self):
+        return self._save_model(self.semantic_transformer)
+
+
+class AcousticTrainer(Trainer):
+    """Trainer class derived from `Trainer`."""
+
+    def __init__(
+        self,
+        semantic_encoder: W2VHuBERT_Quantizier,
+        semantic_transformer: TransformerDecoderOnly,
+        acoustic_enc_dec: CustomEncodecModel,
+        acoustic_transformer: TransformerDecoderOnly,
+        train_dataloader: AudioDataLoader,
+        val_dataloader: AudioDataLoader,
+        test_dataloader: AudioDataLoader,
+        loss: nn.Module,
+        optimizer: torch.optim.Optimizer,
+        intervals: int,
+        save_path: Path,
+        early_stop_counter: int,
+        early_stopping_range: int,
+        epochs: int,
+    ):
+        """
+        Takes as input `semantic_encoder` and `semantic_transformer`.
+        They determine the `semantic_modelling`.
+
+        `semantic_encoder` must be trained ahead of time, this trainer only
+        trains `semantic_transformer`.
+
+        Args
+        ----
+            `semantic_encoder` (W2VHuBERT_Quantizier)
+
+            `semantic_transformer` (TransformerDecoderOnly)
+
+            `train_dataloader` (AudioDataLoader)
+
+            `val_dataloader` (AudioDataLoader)
+
+            `test_dataloader` (AudioDataLoader)
+
+            `loss` (nn.Module)
+
+            `optimizer` (torch.optim.Optimizer)
+
+            `intervals` (int)
+
+            `save_path` (Path)
+
+            `early_stop_counter` (int)
+
+            `early_stopping_range` (int)
+
+            `epochs` (int)
+        """
+        super().__init__(
+            semantic_encoder=semantic_encoder,
+            semantic_transformer=semantic_transformer,
+            acoustic_enc_dec=acoustic_enc_dec,
+            acoustic_transformer=acoustic_transformer,
+            train_dataloader=train_dataloader,
+            val_dataloader=val_dataloader,
+            test_dataloader=test_dataloader,
+            loss=loss,
+            optimizer=optimizer,
+            intervals=intervals,
+            save_path=save_path,
+            early_stop_counter=early_stop_counter,
+            early_stopping_range=early_stopping_range,
+            epochs=epochs,
+        )
+
+    def loss_generator(self, batch):
+        # TODO: Finish, but wait for valerio.
+        semantic_encode = self.semantic_encoder(batch)
+        semantic_token = self.semantic_transformer(semantic_encode)
+        acoustic_tokens = self.acoustic_enc_dec.encode(batch)
+        conditioning = torch.cat((semantic_token, acoustic_tokens), dim=1)
+
+        return self.acoustic_transformer.fit(conditioning)
+
+    def train(self):
+        return self._train(self.acoustic_transformer)
+
+    def validate(self):
+        return self._validate(self.acoustic_transformer)
+
+    def test(self):
+        return self._test(self.acoustic_transformer)
+
+    def save_checkpoint(self, epoch):
+        return self._save_checkpoint(self.acoustic_transformer, epoch)
+
+    def load_checkpoint(self, epoch):
+        return self._load_checkpoint(self.acoustic_transformer, epoch)
+
+    def save_model(self):
+        return self._save_model(self.acoustic_transformer)
---
+++ tests/test_trainer.py
@@ -0,0 +1,119 @@
+"""Unittests for trainer class."""
+
+import os
+import unittest
+import unittest.test
+import warnings
+from pathlib import Path
+
+import torch
+from torch import nn
+
+from audiolm.data_preparation import AudioDataLoader
+from audiolm.semantic_acoustic_modeling.W2VHuBERT_Quantizier import W2VHuBERT_Quantizier
+from audiolm.transformer.AbsoluteTransformer import TransformerDecoderOnly
+from audiolm.transformer.trainer import SemanticTrainer, AcousticTrainer
+from audiolm.acoustic_modelling.custom_encodec import CustomEncodecModel
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+torch.set_warn_always(False)
+
+
+class TestSemanticEncoder(unittest.TestCase):
+    def test_end2end(self):
+        """Test if the pipeline generate a single emebeding"""
+        print("===========================================")
+        print("End to end Pipeline for semantic modelling.")
+        print("===========================================")
+
+        print("Created generator.")
+        semantic_encoder = W2VHuBERT_Quantizier()
+        print("Created encoder.")
+        semantic_transformer = TransformerDecoderOnly(
+            500,  # Valore massimo del quantizzatore
+            768,  # Numero arbitratio
+        )
+        print("Created transformer.")
+        dataloader = AudioDataLoader(os.getcwd() + "\\data\\datasets")
+        print("Fetched Dataloader and divided in train, test and validation.")
+        loss = nn.CrossEntropyLoss()
+        print("Created cross entropy loss")
+        optimizer = torch.optim.Adam(semantic_transformer.parameters(), lr=0.001)
+        print("Adam")
+        intervals = 10
+        save_path = Path(os.getcwd() + "\\data\\models")
+        early_stop_counter = 10
+        early_stopping_range = 10
+        epochs = 10
+        semantic_trainer = SemanticTrainer(
+            semantic_encoder=semantic_encoder,
+            semantic_transformer=semantic_transformer,
+            train_dataloader=dataloader,
+            val_dataloader=None,
+            test_dataloader=None,
+            loss=loss,
+            optimizer=optimizer,
+            intervals=intervals,
+            save_path=save_path,
+            early_stop_counter=early_stop_counter,
+            early_stopping_range=early_stopping_range,
+            epochs=epochs,
+        )
+
+        semantic_trainer.train()
+
+
+class TestAcousticEncoder(unittest.TestCase):
+    def train_end2end(self):
+        """Test if the pipeline generate a single emebeding"""
+        print("===========================================")
+        print("End to end Pipeline for coarse acoustic training.")
+        print("===========================================")
+
+        print("Created generator.")
+        semantic_encoder = W2VHuBERT_Quantizier()
+        print("Created encoder.")
+        semantic_transformer = TransformerDecoderOnly(
+            500,  # Valore massimo del quantizzatore
+            768,  # Numero arbitratio
+        )
+        acoustic_enc_dec = CustomEncodecModel()
+        print("Created semantic transformer.")
+        acoustic_transfomer = TransformerDecoderOnly(
+            1024,  # Valore massimo del quantizzatore
+            1024,  # Numero arbitratio
+        )
+        print("Created acoustic transformer.")
+        dataloader = AudioDataLoader(os.getcwd() + "\\data\\datasets")
+        print("Fetched Dataloader and divided in train, test and validation.")
+        loss = nn.CrossEntropyLoss()
+        print("Created cross entropy loss")
+        optimizer = torch.optim.Adam(semantic_transformer.parameters(), lr=0.001)
+        print("Adam")
+        intervals = 10
+        save_path = Path(os.getcwd() + "\\data\\models")
+        early_stop_counter = 10
+        early_stopping_range = 10
+        epochs = 10
+        semantic_trainer = AcousticTrainer(
+            semantic_encoder=semantic_encoder,
+            semantic_transformer=semantic_transformer,
+            acoustic_enc_dec=acoustic_enc_dec,
+            acoustic_transformer=acoustic_transformer,
+            train_dataloader=dataloader,
+            val_dataloader=None,
+            test_dataloader=None,
+            loss=loss,
+            optimizer=optimizer,
+            intervals=intervals,
+            save_path=save_path,
+            early_stop_counter=early_stop_counter,
+            early_stopping_range=early_stopping_range,
+            epochs=epochs,
+        )
+
+        semantic_trainer.train()
+
+
+if __name__ == "__main__":
+    unittest.main()
